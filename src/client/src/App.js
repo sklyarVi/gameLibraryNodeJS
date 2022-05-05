53c25bc11f2f81b4d137d51d5c6781432a8c1563
sklyarVi <sklyar.vitaliy@gmail.com>
2022-05-05 20:18:37 +0200

    add some pages, import css styles

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,10 +1,13 @@
-import {React} from 'react'
+import React from 'react'
+import {Route} from "react-router-dom";
+import 'materialize-css'
 
 function App() {
     return(
-        <div>
+        <div className="container">
             <h1>Hello</h1>
         </div>
     )
 }
+
 export default App;
---
+++ src/client/src/pages/Games.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import {Table} from 'semantic-ui-react'
+
+export const Games = () => {
+    return (
+        <div className="container">
+            <h1>Games Page</h1>
+        </div>
+    )
+}
